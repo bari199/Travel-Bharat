c8beb19894de977359a83e420a5408a0a424e860
Md Abdul Bari <bari1999smab@gmail.com>
2026-07-22 06:53:50 -0700

    bug fixed phase level 5

--- frontend/src/components/Home/Navbar.jsx
+++ frontend/src/components/Home/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "axios";
+import api from "@/lib/api";
 import { useNavigate, Link, useLocation } from "react-router-dom";
 import {
   BookA,
