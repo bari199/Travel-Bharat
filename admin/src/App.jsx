bd5886eba6c7beab1aeff4b096f3adc7756e445d
Md Abdul Bari <bari1999smab@gmail.com>
2026-07-22 14:24:31 -0700

    Modify files and fixed bugs 5

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/admin/Login";
 import Dashboard from "./pages/admin/Dashboard";
