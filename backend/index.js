e497c3abe643fa742f31538924266215cf371054
Md Abdul Bari <bari1999smab@gmail.com>
2026-07-24 04:48:38 -0700

    bux fixed

--- backend/index.js
+++ backend/index.js
@@ -40,15 +40,16 @@ const PORT = process.env.PORT || 8000;
 |--------------------------------------------------------------------------
 */
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "https://travel-bharat-e639.vercel.app",
-    "https://travel-bharata.onrender.com",
-  ],
-  credentials: true,
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+
+  // Frontend (Vercel)
+  "https://travel-bharat-e639.vercel.app",
+
+  // Admin (Render)
+  "https://travel-bharata.onrender.com",
+];
 
 /*
 |--------------------------------------------------------------------------
